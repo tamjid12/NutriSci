6a52f088b70c4f70100737990d3399ea1dd14dbd
Akid Tamjid Rahman <tamjid12@my.yorku.ca>
2025-06-11 22:38:39 -0400

    DB connection
    Written DB connection code

---
+++ src/ca/yorku/eecs3311/db/DBTest.java
@@ -0,0 +1,23 @@
+package ca.yorku.eecs3311.db;
+
+import java.sql.*;
+
+public class DBTest {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/nutriscidb", "root", "Tamjid01711!");
+
+            System.out.println("Connected to nutriscidb!");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT VERSION();");
+            while (rs.next()) {
+                System.out.println("MySQL Version: " + rs.getString(1));
+            }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
