564086938277567440c7c754511fddb95df714bb
Nusayba <122335031+nusaybahossain@users.noreply.github.com>
2025-07-23 00:02:44 -0400

    Update MealLogController.java

--- src/ca/yorku/eecs3311/meal/MealLogController.java
+++ src/ca/yorku/eecs3311/meal/MealLogController.java
@@ -1,36 +1,22 @@
-// src/ca/yorku/eecs3311/meal/MealLogController.java
 package ca.yorku.eecs3311.meal;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MealLogController {
-    private MealEntryDAO dao = new MealEntryDAO();
+    private final MealEntryDAO dao = new MealEntryDAO();
+    private final MealDAO mealDAO = new MealDAO();
 
-    /**
-     * Save a full MealEntry (with items)
-     */
     public boolean saveMeal(MealEntry entry) {
         return dao.save(entry);
     }
 
-    /**
-     * Get all MealEntries for a user on a given date
-     */
     public List<MealEntry> getMealsForUserOnDate(String profile, LocalDate date) {
         return dao.findByProfileAndDate(profile, date);
     }
 
-    public List<MealEntry> getMealsForUserBetweenDates(String profileName, LocalDate start, LocalDate end) throws SQLException, SQLException {
-        MealDAO dao = new MealDAO();
-        try {
-            return dao.getMealsForUserBetweenDates(profileName, start, end);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public List<MealEntry> getMealsForUserBetweenDates(String profileName, LocalDate start, LocalDate end) throws SQLException {
+        return mealDAO.getMealsForUserBetweenDates(profileName, start, end); // mealDAO now uses "id" instead of entry_id
     }
 }
