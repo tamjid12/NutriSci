6c56520bedb44350e51ac17aac74215b753c05bc
Nusayba <122335031+nusaybahossain@users.noreply.github.com>
2025-07-22 22:59:43 -0400

    Create MealItemDAO.java

---
+++ src/ca/yorku/eecs3311/meal/MealItemDAO.java
@@ -0,0 +1,22 @@
+package ca.yorku.eecs3311.meal;
+
+import java.sql.*;
+
+public class MealItemDAO {
+    private static final String URL = "jdbc:mysql://localhost:3306/nutriscidb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Ravenclaw16.";
+
+    public void updateMealItemName(int id, String newName) {
+        String sql = "UPDATE MealItem SET food_name = ? WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, newName);
+            ps.setInt(2, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
